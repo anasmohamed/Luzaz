d0cbaa858a821a6f2ac35734977c54c99fad2150
jets <jets@learniosfreecoms-mac.local>
2019-08-03 01:49:28 +0300

    Initial Commit

---
+++ Luzaz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Luzaz
+//
+//  Created by jets on 12/1/1440 AH.
+//  Copyright © 1440 AH Luzaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
