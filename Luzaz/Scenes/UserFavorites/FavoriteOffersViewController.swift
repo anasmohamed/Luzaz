066fe7686c1036ee857bd0516b7690598ebb5ed1
anasmohamed <anaskamal659@gmail.com>
2019-09-24 03:06:03 +0300

    save work

--- Luzaz/Scenes/UserFavorites/FavoriteOffersViewController.swift
+++ Luzaz/Scenes/UserFavorites/FavoriteOffersViewController.swift
@@ -11,40 +11,37 @@ import UIKit
 class FavoriteOffersViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     // MARK: Outlets
     @IBOutlet weak var tableView: UITableView!
-
+    
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     var presenter: FavoriteOffersPresneter!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
         presenter = FavoriteOffersPresneter(view: self)
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        presenter.viewDidLoad()
+    }
     func setupTableView() {
         tableView.register(UINib(nibName: "FavoriteOffersTableViewCell", bundle: nil), forCellReuseIdentifier: "FavoriteOffersTableViewCell")
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-       
-            return presenter.getFavoriteOffersCount()
-     
-    
-        
-        
+        return presenter.getFavoriteOffersCount()
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -58,12 +55,11 @@ class FavoriteOffersViewController: UIViewController,UITableViewDelegate,UITable
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteOffersTableViewCell", for: indexPath) as! FavoriteOffersTableViewCell
         presenter.configure(cell: cell, for: indexPath.row)
-        
         return cell
     }
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
-   
+    
+    
 }
