c1e3ff0e637e591dd1a5218b2ea2e2b1e817af71
Anas Mohamed <anaskamal659@gmail.com>
2019-11-22 11:41:47 +0200

    some edit in view

--- Luzaz/Scenes/SellYourItems/EndViewController/SellYourItemEndViewController.swift
+++ Luzaz/Scenes/SellYourItems/EndViewController/SellYourItemEndViewController.swift
@@ -86,7 +86,7 @@ class SellYourItemEndViewController: UIViewController,SellYourItemEndView {
     }
     
     @IBAction func finishBtnWasPressed(_ sender: Any) {
-        if !(emailTextView.text?.isEmpty)! && !(fullNameTextView.text?.isEmpty)! && !(mobileNumberTextView.text?.isEmpty )! {
+        if !(emailTextView.text?.isEmpty)! && !(fullNameTextView.text?.isEmpty)! && !(mobileNumberTextView.text!.isEmpty && isEditProduct) {
              presenter.addUserOffer(token:UserDefaults.standard.string(forKey: "token")!,privacy_policy:"1",id_governate:city!,id_category:category!,id_sub_category:subCategory!,attr:(attributeIds?.joined(separator: ","))!,attr_values:(attributeValues?.joined(separator: ","))!,title:offerTitle!,id_brand:brand!,offer_type:condition!,decription:offerDescription,price:price!,discount_prec:discount!,youtube_link:"",reseller_name:fullNameTextView.text!,reseller_phone:mobileNumberTextView.text!,reseller_mail:(emailTextView.text)!,contact_type:contactType!,image:offerImage!,album:offerAlbum,lat: lat ?? "",long: long ?? "")
             
         }else if(!(emailTextView.text?.isEmpty)! && !(fullNameTextView.text?.isEmpty)! && !(mobileNumberTextView.text!.isEmpty && isEditProduct))
