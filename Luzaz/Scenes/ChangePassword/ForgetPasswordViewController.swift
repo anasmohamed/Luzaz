2f68a8ef85fc6a525254833810ae7b761a2d1a0e
Anas Mohamed <anaskamal659@gmail.com>
2019-12-10 17:03:41 +0200

    save work

---
+++ Luzaz/Scenes/ChangePassword/ForgetPasswordViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ForgetPasswordViewController.swift
+//  Luzaz
+//
+//  Created by Anas Mohamed on 12/10/19.
+//  Copyright © 2019 Luzaz. All rights reserved.
+//
+
+import UIKit
+
+class ForgetPasswordViewController: ViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func forgetPasswordBtnWasPressed(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
